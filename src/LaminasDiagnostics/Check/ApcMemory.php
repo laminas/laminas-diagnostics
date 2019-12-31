dec28fa941e5a455387e527f7507d608070b6163
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:42:07 -0600

    Rewriting as Laminas Project package

---
+++ src/LaminasDiagnostics/Check/ApcMemory.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-diagnostics for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-diagnostics/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-diagnostics/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Laminas\Diagnostics\Check;
+
+use Laminas\Diagnostics\Result\Failure;
+use Laminas\Diagnostics\Result\Skip;
+use Laminas\Diagnostics\Result\Success;
+use Laminas\Diagnostics\Result\Warning;
+
+/**
+ * Checks to see if the APC memory usage is below warning/critical thresholds
+ *
+ * APC memory logic borrowed from APC project:
+ *      https://github.com/php/pecl-caching-apc/blob/master/apc.php
+ *      authors:   Ralf Becker <beckerr@php.net>, Rasmus Lerdorf <rasmus@php.net>, Ilia Alshanetsky <ilia@prohost.org>
+ *      license:   The PHP License, version 3.01
+ *      copyright: Copyright (c) 2006-2011 The PHP Group
+ */
+class ApcMemory extends AbstractMemoryCheck
+{
+    /**
+     * APC information
+     *
+     * @var array
+     */
+    private $apcInfo;
+
+    /**
+     * Perform the check
+     *
+     * @see \Laminas\Diagnostics\Check\CheckInterface::check()     *
+     * @return Failure|Skip|Success|Warning
+     */
+    public function check()
+    {
+        if (!ini_get('apc.enabled')) {
+            return new Skip('APC has not been enabled or installed.');
+        }
+
+        if (php_sapi_name() == 'cli' && !ini_get('apc.enabled_cli')) {
+            return new Skip('APC has not been enabled in CLI.');
+        }
+
+        if (!function_exists('apc_sma_info')) {
+            return new Warning('APC extension is not available');
+        }
+
+        if (!$this->apcInfo = apc_sma_info()) {
+            return new Warning('Unable to retrieve APC memory status information.');
+        }
+
+        return parent::check();
+    }
+
+    /**
+     * Returns the total memory in bytes
+     *
+     * @return int
+     */
+    protected function getTotalMemory()
+    {
+        return $this->apcInfo['num_seg'] * $this->apcInfo['seg_size'];
+    }
+
+    /**
+     * Returns the used memory in bytes
+     *
+     * @return int
+     */
+    protected function getUsedMemory()
+    {
+        return $this->getTotalMemory() - $this->apcInfo['avail_mem'];
+    }
+}
---
+++ src/LaminasDiagnostics/Check/HttpService.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-diagnostics for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-diagnostics/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-diagnostics/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Laminas\Diagnostics\Check;
+
+use Laminas\Diagnostics\Result\Failure;
+use Laminas\Diagnostics\Result\Success;
+
+/**
+ * Attempt connection to given HTTP host and (optionally) check status code and page content.
+ */
+class HttpService extends AbstractCheck
+{
+    /**
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * @var int
+     */
+    protected $port;
+
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * @var int
+     */
+    protected $statusCode;
+
+    /**
+     * @var int
+     */
+    protected $content;
+
+    /**
+     * @param string $host       Host name or IP address to check.
+     * @param int    $port       Port to connect to (defaults to 80)
+     * @param string $path       The path to retrieve (defaults to /)
+     * @param int    $statusCode (optional) Expected status code
+     * @param null   $content    (optional) Expected substring to match agains the page content.
+     */
+    public function __construct($host, $port = 80, $path = '/', $statusCode = null, $content = null)
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->path = $path;
+        $this->statusCode = $statusCode;
+        $this->content = $content;
+    }
+
+    /**
+     * @see Laminas\Diagnostics\CheckInterface::check()
+     */
+    public function check()
+    {
+        $fp = @fsockopen($this->host, $this->port, $errno, $errstr, 10);
+        if (!$fp) {
+            return new Failure(sprintf('No http service running at host %s on port %s', $this->host, $this->port));
+        }
+
+        $header = "GET {$this->path} HTTP/1.0\r\n";
+        $header .= "Host: {$this->host}\r\n";
+        $header .= "Connection: close\r\n\r\n";
+        fputs($fp, $header);
+        $str = '';
+        while (!feof($fp)) {
+            $str .= fgets($fp, 1024);
+        }
+        fclose($fp);
+
+        if ($this->statusCode && !preg_match("/^HTTP\/[0-9]\.[0-9] {$this->statusCode}/", $str)) {
+            return new Failure("Status code {$this->statusCode} does not match response from {$this->host}:{$this->port}{$this->path}");
+        }
+
+        if ($this->content && strpos($str, $this->content) === false) {
+            return new Failure("Content {$this->content} not found in response from {$this->host}:{$this->port}{$this->path}");
+        }
+
+        return new Success();
+    }
+}
