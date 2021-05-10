1557e3f173fd00291d286b3ea1b98454c21b08d4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:06:24 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/GuzzleHttpServiceTest.php
+++ test/GuzzleHttpServiceTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-diagnostics for the canonical source repository
- * @copyright https://github.com/laminas/laminas-diagnostics/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-diagnostics/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\Diagnostics;
 
 use GuzzleHttp\Client as GuzzleClient;
